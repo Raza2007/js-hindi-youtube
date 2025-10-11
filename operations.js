d22ba3e16c0e0185cd77db348be5b0ec69a11a24
Raza2007 <mohdrazaraza512@gmail.com>
2025-10-11 19:54:30 +0000

    Why string to nmber conversions

---
+++ operations.js
@@ -0,0 +1,49 @@
+// ---------------------::Operations::--------------
+let value = 12;
+let Negvalue = -value;
+// console.log(Negvalue);
+
+// console.log(10+10);
+// console.log(10-10);
+// console.log(10*10);
+// console.log(10/10);
+
+// console.log(10**10);
+// console.log(10%10);
+
+let str1 = "Mohammad ";
+let str2 = "Umar";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log(str1+str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+false);
+console.log(false);
+
+
+let num1,num2,num3
+num1=num2=num3=30+12
+console.log(num1);
+
+let gameCounter=122
+// gameCounter--
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
