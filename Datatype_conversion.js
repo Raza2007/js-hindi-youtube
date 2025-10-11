46848a8da8b37a6df394ff50a31dd2916f7e212f
Raza2007 <mohdrazaraza512@gmail.com>
2025-10-11 19:21:15 +0000

    container config done

---
+++ Datatype_conversion.js
@@ -0,0 +1,38 @@
+// let score= "19umar"
+// let score="22"
+// let score = null
+// let score=true
+
+let score="Ahmad"
+console.log(typeof score);
+
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+"19"-----> 19
+"19umar"---->Nan---->Not an number
+"true"---->1 false-------->0;
+*/
+
+let isLoggedIn=1
+// let isLoggedIn = "Kadir"
+// let isLoggedIn=""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+/*
+1-->ture
+0-->false
+""---->false
+"kadir"---->true
+*/
+let someNumber=222
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
