8a9c8fff4887592928731157b879921b89557924
Raza2007 <mohdrazaraza512@gmail.com>
2025-10-11 18:24:37 +0000

    container config done

---
+++ variables.js
@@ -0,0 +1,26 @@
+const account_Id = 102133
+let accountEmail = "ahmadbhai41188@gmail.com"
+var accountPassword = "08373"
+accountCity = "Aazamgarh"
+
+
+
+// account_Id=2 // not allowed
+accountEmail="kadir512@gmail.com"
+accountCity="Gonda"
+accountPassword="12255"
+let accountState="Delhi"
+
+
+// console.log(accountEmail);
+// console.log(accountCity);
+
+
+// console.log(account_Id);
+console.table([accountEmail,accountCity,accountPassword,accountState])
+
+
+
+
+
+
