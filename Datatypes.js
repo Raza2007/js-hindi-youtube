d1b88bf89e172ec7e02d90f55c51d2d79689c8de
Raza2007 <mohdrazaraza512@gmail.com>
2025-10-11 18:51:06 +0000

    conatiner config done

---
+++ Datatypes.js
@@ -0,0 +1,34 @@
+/*
+"Use strict"; // treat all JS code as newer version
+alert(3+3); // We are using node js,not browser.
+*/
+
+console.log(20+20)
+
+// code readibility that be should high
+console.log("Helllll!");
+
+let name="umar"
+let course="MCA"
+/*
+Datatypes:
+
+Number
+String
+Boolean
+BigInt
+null---> standalone value
+undefined--->
+symbol-->unique
+*/
+
+//object:
+console.log(typeof"Umar");
+console.log(typeof undefined);
+console.log(typeof null); // Obejct
+
+
+
+
+
+
